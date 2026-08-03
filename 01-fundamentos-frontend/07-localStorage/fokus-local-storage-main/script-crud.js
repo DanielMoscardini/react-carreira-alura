6cdd017593b8ea1ec44bf3c10561be41b0f35726
Daniel Moscardini <moscardinibdaniel@gmail.com>
2026-08-03 20:47:05 -0300

    submetendo o formulario

---
+++ 01-fundamentos-frontend/07-localStorage/fokus-local-storage-main/script-crud.js
@@ -0,0 +1,20 @@
+const btnAdicionarTarefa = document.querySelector('.app__button--add-task')
+const formAdicionarTarefa = document.querySelector('.app__form-add-task')
+const textArea = document.querySelector('.app__form-textarea')
+
+const tarefas = []
+
+btnAdicionarTarefa.addEventListener('click', () => {
+    formAdicionarTarefa.classList.toggle('hidden')
+})
+
+formAdicionarTarefa.addEventListener('submit', (evento) => {
+    // não recarrega a página ao enviar o form.
+    evento.preventDefault()
+    
+    const tarefa = {
+        descricao: textArea.value
+    }
+
+    tarefas.push(tarefa)
+})
