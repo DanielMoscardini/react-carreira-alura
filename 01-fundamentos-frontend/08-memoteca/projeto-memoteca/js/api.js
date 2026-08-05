098ca7f3b1745f48d5964c199b46b5d6ec86f535
Daniel Moscardini <moscardinibdaniel@gmail.com>
2026-08-05 15:47:16 -0300

    finalizando método POST

--- 01-fundamentos-frontend/08-memoteca/projeto-memoteca/js/api.js
+++ 01-fundamentos-frontend/08-memoteca/projeto-memoteca/js/api.js
@@ -9,6 +9,25 @@ const api = {
             alert('Erro ao buscar pensamentos.')
             throw error
         }
+    },
+
+    async salvarPensamento(pensamento) {
+        try {
+
+            const response = await fetch('http://localhost:3000/pensamentos', {
+                method: 'POST', // enviar informação'
+                headers: {
+                    'Content-Type': 'application/json' // tipo de conteudo enviado na requisição
+                },
+                body: JSON.stringify(pensamento) // converte de JS para string JSON
+            
+            })
+            return await response.json()
+
+        } catch {
+            alert('Erro ao buscar pensamentos.')
+            throw error
+        }
     }
 }
 
--- 01-fundamentos-frontend/08-memoteca/projeto-memoteca/js/main.js
+++ 01-fundamentos-frontend/08-memoteca/projeto-memoteca/js/main.js
@@ -1,5 +1,34 @@
 import ui from './ui.js'
+import api from './api.js'
+
+const botaoCancelar = document.querySelector('#botao-cancelar')
 
 document.addEventListener('DOMContentLoaded', () => {
     ui.renderizarPensamentos()
+
+    const formularioPensamento = document.querySelector('#pensamento-form')
+    formularioPensamento.addEventListener('submit', manipularSubmissaoFormulario)
+
+    botaoCancelar.addEventListener('click', manipularCancelamento)
 })
+
+function manipularCancelamento() {
+    ui.limparFormulario()
+}
+
+async function manipularSubmissaoFormulario(event) {
+    event.preventDefault()
+    const id = document.querySelector('#pensamento-id').value
+    const conteudo = document.querySelector('#pensamento-conteudo').value
+    const autoria = document.querySelector('#pensamento-autoria').value
+    
+
+    try {
+        await api.salvarPensamento({ conteudo, autoria })
+        ui.renderizarPensamentos()
+    } catch {
+        alert('Erro ao salvar pensamentos')
+    }
+}
+
+
