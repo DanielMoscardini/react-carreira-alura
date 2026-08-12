17ccd99e7481de415fc7f365d7ae3276fd0154ef
Daniel Moscardini <moscardinibdaniel@gmail.com>
2026-08-12 16:14:05 -0300

    adicionando temas

--- 02-react-jsx/01-tecboard/src/App.jsx
+++ 02-react-jsx/01-tecboard/src/App.jsx
@@ -1,19 +1,71 @@
 import "./App.css";
+import { Banner } from "./components/Banner";
 import { FormularioDeEvento } from "./components/FormularioDeEvento";
+import { Tema } from "./components/Tema";
 
 // no React, componentes são FUNÇÕES.
 
 function App() {
+  const temas = [
+    {
+      id: 1,
+      nome: "front-end",
+    },
+    {
+      id: 2,
+      nome: "back-end",
+    },
+    {
+      id: 3,
+      nome: "devops",
+    },
+    {
+      id: 4,
+      nome: "inteligência artificial",
+    },
+    {
+      id: 5,
+      nome: "data science",
+    },
+    {
+      id: 6,
+      nome: "cloud",
+    },
+  ];
+
   return (
     <main>
       <header>
         <img src="/logo.png" alt="Logo" />
       </header>
-      <section>
-        <img src="/banner.png" alt="Banner" />
-      </section>
+
+      <Banner />
 
       <FormularioDeEvento />
+
+      <section>
+        <Tema tema={temas[0]} />
+      </section>
+
+      <section>
+        <Tema tema={temas[1]} />
+      </section>
+
+      <section>
+        <Tema tema={temas[2]} />
+      </section>
+
+      <section>
+        <Tema tema={temas[3]} />
+      </section>
+
+      <section>
+        <Tema tema={temas[4]} />
+      </section>
+
+      <section>
+        <Tema tema={temas[5]} />
+      </section>
     </main>
   );
 }
