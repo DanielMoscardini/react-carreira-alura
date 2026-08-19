231787785645330f923f8f1652c4fefcb90932f7
Daniel Moscardini <moscardinibdaniel@gmail.com>
2026-08-19 17:48:29 -0300

    finalizando tailwind

--- 02-react-jsx/04-poupaapp-tailwind/src/App.jsx
+++ 02-react-jsx/04-poupaapp-tailwind/src/App.jsx
@@ -1,9 +1,50 @@
 import { Aside } from "./Components/Aside";
+import Card from "./Components/Card";
+import { Container } from "./Components/Container";
+import { DailyBudget } from "./Components/DailyBudget";
+import { Main } from "./Components/Main";
+import { SearchInput } from "./Components/SearchInput";
+import { Typography } from "./Components/Typography";
 
 function App() {
   return (
     <div className="min-h-screen bg-neutral-background">
-      <Aside />
+      <Container>
+        <Aside />
+        <Main>
+          <SearchInput />
+          <div>
+            <Typography variant="h1">Olá, Daniel</Typography>
+            <Typography variant="body">
+              Veja como estão suas finanças hoje.
+            </Typography>
+          </div>
+
+          <section>
+            <Card>
+              <Card.Header>Orçamento diário disponível</Card.Header>
+              <Card.Body>
+                <DailyBudget value={42} />
+              </Card.Body>
+            </Card>
+
+            <Card>
+              <Card.Header>Progresso da meta financeira</Card.Header>
+              <Card.Body>200</Card.Body>
+            </Card>
+
+            <Card>
+              <Card.Header>Movimentação financeira</Card.Header>
+              <Card.Body>200</Card.Body>
+            </Card>
+
+            <Card>
+              <Card.Header>Minhas contas</Card.Header>
+              <Card.Body>200</Card.Body>
+            </Card>
+          </section>
+        </Main>
+      </Container>
     </div>
   );
 }
