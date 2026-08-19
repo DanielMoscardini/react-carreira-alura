d5e66d6759c0a8449363535ccc797525c8174341
Daniel Moscardini <moscardinibdaniel@gmail.com>
2026-08-19 16:30:10 -0300

    iniciando poupapp com tailwind

---
+++ 02-react-jsx/04-poupaapp-tailwind/src/App.jsx
@@ -0,0 +1,11 @@
+import { Aside } from "./Components/Aside";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-neutral-background">
+      <Aside />
+    </div>
+  );
+}
+
+export default App;
