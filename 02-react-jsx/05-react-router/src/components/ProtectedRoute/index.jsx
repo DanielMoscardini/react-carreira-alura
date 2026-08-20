b2c13c57f2d2dffd6789c4945caa3957e207d9f0
Daniel Moscardini <moscardinibdaniel@gmail.com>
2026-08-19 21:36:06 -0300

    finalizando react router

---
+++ 02-react-jsx/05-react-router/src/components/ProtectedRoute/index.jsx
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+import { useAuth } from "../../hooks/useAuth";
+import { Spinner } from "../Spinner";
+import { useNavigate } from "react-router-dom";
+
+export const ProtectedRoute = ({ children }) => {
+  const { isAuthenticated, isLoading } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isLoading && !isAuthenticated) {
+      navigate("/auth/login");
+    }
+  }, [isLoading, isAuthenticated, navigate]);
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  if (!isAuthenticated) {
+    return null;
+  }
+
+  return children;
+};
