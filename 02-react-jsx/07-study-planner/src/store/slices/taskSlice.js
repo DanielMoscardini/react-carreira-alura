0f0b34604ebc561abe80fd0cad5ac648c9409ce1
Daniel Moscardini <moscardinibdaniel@gmail.com>
2026-08-20 14:29:51 -0300

    finalizando redux

--- 02-react-jsx/07-study-planner/src/store/slices/taskSlice.js
+++ 02-react-jsx/07-study-planner/src/store/slices/taskSlice.js
@@ -1,7 +1,26 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  tasks: [],
+  tasks: [
+    {
+      id: 1,
+      title: "Task 1",
+      description: "Description 1",
+      completed: false,
+    },
+    {
+      id: 2,
+      title: "Task 2",
+      description: "Description 2",
+      completed: false,
+    },
+    {
+      id: 3,
+      title: "Task 3",
+      description: "Description 3",
+      completed: true,
+    },
+  ],
 };
 
 const taskSlice = createSlice({
@@ -20,5 +39,27 @@ const taskSlice = createSlice({
       const task = state.tasks.find((task) => task.id === action.payload);
       if (task) task.completed = !task.completed;
     },
+    editTask: (state, action) => {
+      const { taskId, updatedTask } = action.payload;
+      const taskIndex = state.tasks.findIndex((task) => task.id === taskId);
+      if (taskIndex !== -1) {
+        state.tasks[taskIndex] = { ...state.tasks[taskIndex], ...updatedTask };
+      }
+    },
+    deleteTask: (state, action) => {
+      state.tasks = state.tasks.filter((task) => task.id !== action.payload);
+    },
   },
 });
+
+export const { addTask, toggleTaskComplete, editTask, deleteTask } =
+  taskSlice.actions;
+
+export const selectTasks = (state) => state.tasks.tasks;
+export const selectPendingTasks = (state) =>
+  state.tasks.tasks.filter((task) => !task.completed);
+
+export const selectCompletedTasks = (state) =>
+  state.tasks.tasks.filter((task) => task.completed);
+
+export default taskSlice.reducer;
